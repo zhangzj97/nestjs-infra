19166b14b5578d4764a6b199bb427253fe820cb3
DylanZhang <987883389@qq.com>
2026-07-06 18:24:16 +0800

    chore

---
+++ src/interface/index.ts
@@ -0,0 +1,61 @@
+import { JwtService as NestJwtService } from "@nestjs/jwt";
+import { Redis } from "ioredis";
+import { DataSource, EntityManager } from "typeorm";
+
+export abstract class JwtService extends NestJwtService {}
+
+export abstract class CacheService {
+  abstract data: Redis;
+  abstract init(): Promise<void>;
+}
+
+export abstract class OrmService {
+  abstract data: DataSource;
+  abstract init(): Promise<void>;
+}
+
+export type UserPayload = {
+  userId: string;
+  username: string;
+};
+
+export abstract class LoggerService {
+  abstract error(message: string, data: unknown): void;
+  abstract info(message: string, data: unknown): void;
+  abstract warn(message: string, data: unknown): void;
+  abstract debug(message: string, data: unknown): void;
+  abstract backup(message: string, data: unknown): void;
+}
+
+export type AccessLog = {
+  requestId: string;
+  path: string;
+  method: string;
+  ip: string;
+  body: Record<string, unknown>;
+  status: string;
+  duration: number;
+  error: {
+    name: string;
+    stack: string;
+    message: string;
+    cause: string;
+  } | null;
+};
+
+export abstract class AlsService {
+  abstract get(): AlsData;
+  abstract update(data: Partial<AlsData>): void;
+
+  abstract run<T>(store: AlsData, callback: () => T): T;
+}
+
+export type AlsData = {
+  requestId: string;
+  startTime: string;
+  success: boolean;
+  userPayload: UserPayload | null;
+  data: Record<string, unknown>;
+
+  entityManager: EntityManager | null;
+};
