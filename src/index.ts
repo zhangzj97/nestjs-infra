3daa2c012f787f0e983c6af0327aa86d1392ffea
DylanZhang <987883389@qq.com>
2026-07-07 08:07:02 +0800

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-export {};
-import {} from "@zzj/ts-utils";
+import { DynamicModule } from "@nestjs/common";
+import { EntitySchema } from "typeorm";
+
+import { AlsModule } from "./provider/als";
+import { CacheModule } from "./provider/cache";
+import { JwtModule } from "./provider/jwt";
+import { LoggerModule } from "./provider/logger";
+import { OrmModule } from "./provider/orm";
+
+export {
+  AlsService, //
+  JwtService, //
+  OrmService, //
+  CacheService, //
+  LoggerService, //
+} from "./interface";
+
+export class InfraModule {
+  static forRoot(options: { entityList: EntitySchema[] }): DynamicModule {
+    return {
+      imports: [
+        AlsModule.forRoot({}), //
+        CacheModule.forRoot({}), //
+        JwtModule.forRoot({}), //
+        OrmModule.forRoot({
+          entityList: options.entityList,
+        }), //
+        LoggerModule.forRoot({}), //
+      ],
+      providers: [],
+      exports: [
+        AlsModule, //
+        CacheModule, //
+        JwtModule, //
+        OrmModule, //
+        LoggerModule, //
+      ],
+      module: InfraModule,
+    };
+  }
+}
